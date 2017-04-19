bb7ecb66ff12de097c6996f98876e1e25705db6d
Jon Uhal <jon@blackrhinoconsulting.com>
2017-04-19 00:16:19 -0800

    Fixed a number of issues related to spree / stripe synchronization.

--- app/models/concerns/spree/recurring/stripe_recurring/api_handler/plan_api_handler.rb
+++ app/models/concerns/spree/recurring/stripe_recurring/api_handler/plan_api_handler.rb
@@ -6,21 +6,22 @@ module Spree
           def create_plan(plan)
             raise_invalid_object_error(plan, Spree::Plan)
             begin
-              if retrieve_api_plan(plan).nil?
-                Stripe::Plan.create(
-                    amount: stripe_amount(plan.amount),
-                    interval: plan.interval,
-                    interval_count: plan.interval_count,
-                    name: plan.name,
-                    currency: plan.currency,
-                    id: plan.api_plan_id,
-                    trial_period_days: plan.trial_period_days
-                )
-              end
+              plan_exists = retrieve_api_plan(plan)
             rescue Stripe::InvalidRequestError => e
               # if retrieve_api_plan(plan) cannot find a plan, don't throw the error.
               # otherwise, rethrow the error.
               raise e unless e.http_status == 404
+            end
+            if not plan_exists
+              Stripe::Plan.create(
+                  amount: stripe_amount(plan.amount),
+                  interval: plan.interval,
+                  interval_count: plan.interval_count,
+                  name: plan.name,
+                  currency: plan.currency,
+                  id: plan.api_plan_id,
+                  trial_period_days: plan.trial_period_days
+              )
             end
           end
 
